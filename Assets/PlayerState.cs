070f05c418ddabe30a99640f60fa62768a11ba12
have-to-do-yeon <kdy0407ll@naver.com>
2024-04-12 14:13:38 +0900

    State Machine Practice

---
+++ Assets/PlayerState.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState
+{
+    protected PlayerStateMachine stateMachine;
+    protected Player player;
+
+    private string animBoolName;
+    
+    public PlayerState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName)
+    {
+        this.stateMachine = _stateMachine;
+        this.player = _player;
+        this.animBoolName = _animBoolName;
+    }
+
+    public virtual void Enter()
+    {
+        player.anim.SetBool(animBoolName, true);
+    }
+
+    public virtual void Update()
+    {
+        Debug.Log("Update " + animBoolName);
+
+    }
+
+    public virtual void Exit()
+    {
+        player.anim.SetBool(animBoolName, false);
+
+    }
+
+}
