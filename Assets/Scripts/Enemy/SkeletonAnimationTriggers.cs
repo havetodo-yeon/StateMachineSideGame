514b785350f9f3982d444cc03af15991d352d797
have-to-do-yeon <kdy0407ll@naver.com>
2024-04-19 17:35:25 +0900

    Skeleton Attack

---
+++ Assets/Scripts/Enemy/SkeletonAnimationTriggers.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SkeletonAnimationTriggers : MonoBehaviour
+{
+    private Enemy_Skeleton enemy => GetComponentInParent<Enemy_Skeleton>();
+
+    private void AnimationTrigger()
+    {
+        enemy.AnimationFinishTrigger();
+    }
+}
